cc00f13fd5be2cde836838341af2dee5215183b2
JulioChura <jchuraaca@unsa.edu.pe>
2025-10-17 08:01:40 -0500

    programa mejorado

--- lab05/ejercicio1.cpp
+++ lab05/ejercicio1.cpp
@@ -7,41 +7,63 @@ using namespace std;
 
 void generarVector(vector<int>& v, int n);
 void imprimirVector(const vector<int>& v);
-void filtrar(const vector<int>& v);
+vector<int> filtrar(const vector<int>& v);
+void mostrarResultado(const vector<int>& filtrados);
 
 int main() {
     srand(time(0));
-    const int n = 10;
+    const int n = 100;
     vector<int> v;
+
+    cout << "PROGRAMA: FILTRAR NUMEROS IMPARES Y MENORES DE 20 " << endl << endl;
+
     generarVector(v, n);
+    cout << "Vector generado (" << n << " elementos):" << endl;
     imprimirVector(v);
-    filtrar(v);
+    cout << endl;
+
+    vector<int> filtrados = filtrar(v);
+    mostrarResultado(filtrados);
+
     return 0;
 }
 
-void generarVector(std::vector<int>& v, int n) {
+void generarVector(vector<int>& v, int n) {
     for (int i = 0; i < n; i++) {
         v.push_back(rand() % 100);
     }
 }
 
-void imprimirVector(const std::vector<int>& v) {
+void imprimirVector(const vector<int>& v) {
     for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << " ";
+        cout << v[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
-void filtrar(const vector<int>& v) {
+vector<int> filtrar(const vector<int>& v) {
+    vector<int> filtrados;
     auto esCorrecto = [](int numero) {
-        return numero % 2 != 0 && numero < 20 ;
+        return numero % 2 != 0 && numero < 20;
     };
-    int count = 0;
-    for ( int n : v ) {
-        if ( esCorrecto(n) ) {
-            count ++;
+    for (int n : v) {
+        if (esCorrecto(n)) {
+            filtrados.push_back(n);
         }
     }
-    cout << "Cantidad: " << count << endl;
+    return filtrados;
+}
 
+void mostrarResultado(const vector<int>& filtrados) {
+    cout << "Cantidad de numeros impares y menores de 20: " << filtrados.size() << endl;
+
+    if (filtrados.empty()) {
+        cout << "No se encontraron numeros impares menores de 20 en el vector." << endl;
+    } else {
+        cout << "Numeros impares menores de 20 encontrados:" << endl;
+        for (int num : filtrados) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
 }
